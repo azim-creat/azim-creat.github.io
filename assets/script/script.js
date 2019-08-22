2b9198ad446b33e2f8a4446f082e5feb5c0fa0f6
azim-creat <53119699+azim-creat@users.noreply.github.com>
2019-08-22 22:27:53 +0600

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.menu-button').click(function(){
+       $('.sideBarBG').slideToggle(500);
+    });
+
+    $(window).resize(function(){
+        if($(window).width()>770){
+            $('.sideBarBG').removeAttr('style');
+        }
+    })
+
+});
